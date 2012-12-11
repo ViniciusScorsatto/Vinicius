97e7808fa9778c15b4f4f5aff5c9ce38782139a8
ViniciusScorsatto <vinicius.scorsatto@gmail.com>
2012-12-11 11:34:23 -0500

    2 mudança

--- TesteOurs/TesteOurs/ViewController.m
+++ TesteOurs/TesteOurs/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSString *teste=@"Teste";
+    NSString *teste1=@"Teste1";
 }
 
 - (void)didReceiveMemoryWarning
